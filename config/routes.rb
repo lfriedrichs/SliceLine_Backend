00ce933d615555b3b2d63f90ce486c6c4e3984e9
123JackCole <123jack.cole@gmail.com>
2020-04-14 09:10:08 -0700

    a few touch ups

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :pizzas
-  resources :orders
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :pizzas
+      resources :orders
+      resources :users
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
