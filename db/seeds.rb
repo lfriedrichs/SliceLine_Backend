b21504d69d221f7b9cb7dce9d2d43271392a3d68
github username <github email address>
2020-04-15 09:27:03 -0700

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,24 +14,18 @@ Topping.destroy_all
 GourmetTopping.destroy_all
 Pizza.destroy_all
 
-sauces = ['tomato', 'white', 'bbq', 'garlic olive oil']
+sauces = ['tomato', 'white', 'bbq', 'garlic olive oil', 'french white', 'lemon hill', 'satay',]
 sauces.map {|sauce| Sauce.create(name: sauce)}
 
-cheeses = ['feta, parmesan, mozzarella, ricota']
+cheeses = ['feta', 'parmesan', 'mozzarella', 'ricota']
 cheeses.map {|cheese| Cheese.create(name: cheese)}
 
-veggies = ['artichoke hearts', 'avocado', 'broccoli', 'capers', 'green peppers', 'mushrooms', 'onions', 'olives', 'roasted garlic', 'spinach', 'sun dried tomatoes', 'zucchini']
-meats = ['bacon, bbq chicken, ham, meatballs, pepperoni, proscuitto, salami, sausage']
+veggies = ['kidney beans', 'asparagus', 'celery', 'garlic', 'red pepper', 'capers', 'cherkin', 'broccoli', 'lime', 'green peppers', 'mushrooms', 'onions', 'olives', 'spinach', 'pineapples']
+meats = ['bacon', 'ham', 'pepperoni', 'salami', 'sausage', 'lamb', 'chicken', 'beef', 'pork']
 toppings = veggies + meats
 toppings.map {|topping| Topping.create(name: topping)}
-
-spices = ['basil, jalapeno peppers, oregano']
-seafood = ['anchovies, shrimps, squid']
+spices = ['basil', 'jalapeno peppers', 'chives', 'sundried tomatoes', 'pepper', 'parsley']
+seafood = ['anchovies', 'shrimp', 'salmon']
 gourmetToppings = spices + seafood
 gourmetToppings.map {|gourmetTopping| GourmetTopping.create(name: gourmetTopping)}
-
-
-
-
-
 
