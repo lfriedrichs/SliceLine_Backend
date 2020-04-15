b3e124275b90a6606d20cdaee7465135d8e1d489
123JackCole <123jack.cole@gmail.com>
2020-04-15 09:40:51 -0700

    jack seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,21 +17,15 @@ Pizza.destroy_all
 sauces = ['tomato', 'white', 'bbq', 'garlic olive oil', 'french white', 'lemon hill', 'satay',]
 sauces.map {|sauce| Sauce.create(name: sauce)}
 
-cheeses = ['feta, parmesan, mozzarella, ricota']
+cheeses = ['feta', 'parmesan', 'mozzarella', 'ricota']
 cheeses.map {|cheese| Cheese.create(name: cheese)}
 
 veggies = ['kidney beans', 'asparagus', 'celery', 'garlic', 'red pepper', 'capers', 'cherkin', 'broccoli', 'lime', 'green peppers', 'mushrooms', 'onions', 'olives', 'spinach', 'pineapples']
-meats = ['bacon, ham, pepperoni, salami, sausage, lamb, chicken, beef, pork']
+meats = ['bacon', 'ham', 'pepperoni', 'salami', 'sausage', 'lamb', 'chicken', 'beef', 'pork']
 toppings = veggies + meats
 toppings.map {|topping| Topping.create(name: topping)}
-ricto
-spices = ['basil, jalapeno peppers, chives, sundried tomatoes, pepper, parsley ']
-seafood = ['anchovies, shrimp, salmon ]
+
+spices = ['basil', 'jalapeno peppers', 'chives', 'sundried tomatoes', 'pepper', 'parsley']
+seafood = ['anchovies', 'shrimp', 'salmon' ]
 gourmetToppings = spices + seafood
 gourmetToppings.map {|gourmetTopping| GourmetTopping.create(name: gourmetTopping)}
-
-
-
-
-
-
